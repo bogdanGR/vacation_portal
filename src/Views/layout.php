2ae955c0000d26dd3464c9f4973cf568d7a2e111
bogdanGR <fant1k450@gmail.com>
2025-08-22 16:41:45 +0300

    remove comments and style.css include in layout file

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -4,9 +4,7 @@
     <meta charset="utf-8">
     <title>Vacation Portal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS (CDN) -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
@@ -27,7 +25,6 @@
     <?= $content ?>
 </main>
 
-<!-- Bootstrap JS (optional) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
